b4ffd9b384e51a51a38fb91c364c54a6baa62e9c
wegeneredv.de <ein.verwegener@gmail.com>
2017-11-05 21:59:48 +0100

    Add files via upload

---
+++ gen/de/wegeneredv/piBENCH/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.wegeneredv.piBENCH;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = false;
+}
---
+++ src/de/wegeneredv/piBENCH/piBENCHActivity.java
@@ -0,0 +1,42 @@
+package de.wegeneredv.piBENCH;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class piBENCHActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
+		WebSettings webSettings = mainWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setDomStorageEnabled(true);
+		webSettings.setLoadWithOverviewMode(true);
+		webSettings.setUseWideViewPort(true);
+		webSettings.setBuiltInZoomControls(true);
+		webSettings.setDisplayZoomControls(false);
+		webSettings.setSupportZoom(true);
+		webSettings.setDefaultTextEncodingName("utf-8");
+        webSettings.setJavaScriptEnabled(true);
+        
+        mainWebView.setWebViewClient(new MyCustomWebViewClient());
+        mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        
+        mainWebView.loadUrl("http://jsfiddle.net/b8zcf4s8/3/");
+    }
+    
+    private class MyCustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
